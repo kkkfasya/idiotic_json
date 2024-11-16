f14a6534c2094225ee65add55a9204637d53ce03
Kamal Fasya <kamalfasya01@gmail.com>
2024-11-17 03:35:31 +0700

    Fix some unnecessary stuff

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
+#!/bin/sh
+
+# Compile & link the libs
 gcc -c microjson-1.7/mjson.c -o mjson.o
 ar rcs libmjson.a mjson.o
 
 gcc -c idioticjson.c -o idioticjson.o
 ar rcs libidioticjson.a idioticjson.o mjson.o
 
-# Compile and link the example program with static libraries
-gcc example.c -static -L. -lidioticjson -o example
+# Compile
+# gcc example.c -L. -lidioticjson -o example
+
+# Statically link lidioticjson, then the rest are dynamically linked
+gcc example.c -L. -Wl,-Bstatic -lidioticjson -Wl,-Bdynamic -o example
 
 # Delete the object file
-rm mjson.o
-rm idioticjson.o
+ rm mjson.o
+ rm idioticjson.o
 # rm *.a # maybe if you want to delete this too
--- example.c
+++ example.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
 #include "idioticjson.h"
 
 int main(int argc, char **argv) {
@@ -10,18 +9,17 @@ int main(int argc, char **argv) {
         int number;
     } Obj;
 
-    Obj *obj = (Obj*) malloc(sizeof(Obj));
+    Obj obj;
     Json_Attr *attr = idiotic_start(sizeof(Obj)); // init this idiotic lib
-    idiotic_append_str_attr(attr, "text", obj->text, sizeof(obj->text)); // idiotic_append_<DATA_TYPE>_attr
-    idiotic_append_bool_attr(attr, "boolean", &(obj->boolean));
-    idiotic_append_int_attr(attr, "number", &(obj->number));
+    idiotic_append_str_attr(attr, "text", obj.text, sizeof(obj.text)); // idiotic_append_<DATA_TYPE>_attr
+    idiotic_append_bool_attr(attr, "boolean", &(obj.boolean));
+    idiotic_append_int_attr(attr, "number", &(obj.number));
     idiotic_end(attr);
     idiotic_parse(argv[1], attr); // after being parsed, it's being freed automatically
 
-    printf("text    : %s\n", obj->text);
-    printf("number  : %d\n", obj->number);
-    printf("boolean : %s\n", obj->boolean ? "true" : "false");
-    free(obj); // dont forget to free
+    printf("text    : %s\n", obj.text);
+    printf("number  : %d\n", obj.number);
+    printf("boolean : %s\n", obj.boolean ? "true" : "false");
     return 0;
 }
 
